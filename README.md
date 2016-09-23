1cf5bb6663ea429e98db39e774305521d51934b9
bgabot89 <bgabot89@gmail.com>
2016-09-22 18:39:06 -0700

    API Working and rendering

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 The goal for this project is to create an application in which the user can find if a certain parking space is available in a parking garage in downtown San Jose
 #Instructions
 The application uses the sanjoseca.gov API: http://data.sanjoseca.gov/dataviews/231073/parking-garage-data/
+#Link to Instructions
+https://github.com/codeforsanjose/Project-Ideas/issues/85
--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
-  //declare a variable from the san jose API
-  var SJSON = 'http://api.data.sanjoseca.gov/api/v2/datastreams/PARKI-GARAG-DATA/data.json/?auth_key=d4e1a9d40c8be33ef960c83f7a9fa88f6250e658&';
+  //declare empty Arrays and strings to be used on the html document
+  var GarageNames = [];
+  var Names = "";
+  var GarageStatus = [];
+  var Status = "";
+  var GarageAvailable = [];
+  var Available = "";
+  var GarageTotal = [];
+  var Total = "";
+  var Sets = "Total Garages";
+
+  //declares a variable that uses the data from the San Jose Parking Garage Data JSON
+  var SJSON = 'http://api.data.sanjoseca.gov/api/v2/datastreams/PARKI-GARAG-DATA/data.ajson/?auth_key=d4e1a9d40c8be33ef960c83f7a9fa88f6250e658&limit=50&';
   $.getJSON(SJSON, function(data) {
-  //parses out each individual entry into the table
-    $.each(data.result.fArray, function(i,f) {
-      var tblRow = "<tr>" + "<td>" + f.fStr + "</td>" + "<td>" + f.fType + "</td>" + "</tr>"
-        $(tblRow).appendTo('#SJData tbody');
-    })
+    // for each entry push into new array
+      for (var i=1; i < data.result.length; i++) {
+        GarageNames.push(data.result[i][0]);
+        GarageStatus.push(data.result[i][1]);
+        GarageAvailable.push(data.result[i][2]);
+        GarageTotal.push(data.result[i][3]);
+      }
+    // increment and append to html
+      for (var j=0; j < GarageNames.length; j++) {
+        console.log(GarageTotal[j]);
+        // Names += "<li>" + GarageNames[j] + "</li>";
+        // Status += "<li>" + GarageStatus[j] + "</li>";
+        // Available += "<li>" + GarageAvailable[j] + "</li>";
+        // Total += "<li>" + GarageTotal[j] + "</li>";
+        Sets += "<li>" + GarageNames[j] + " " +  GarageStatus[j] +  " " + GarageAvailable[j] + " " + GarageTotal[j] + "</li>";
+    }
+    // Garage = Names + " " + " " + Status + " " + Available + " " + Total;
+    document.getElementById("GarageList").innerHTML  = Sets;
   })
-  console.log('page is loaded');
 });
